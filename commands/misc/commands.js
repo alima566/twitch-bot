ae54a4b0203927ed70f4abb8fc281f82a0302246
Aaron Li-Mai <aaronmai@hotmail.com>
2020-11-23 11:40:17 -0700

    check doesn't require hardcoding anymore

--- commands/misc/commands.js
+++ commands/misc/commands.js
@@ -5,13 +5,14 @@ module.exports = {
   commands: "commands",
   description: "A list of all KelleeBot commands",
   cooldown: 15,
+  includeInCommands: false,
   callback: async (client, channel) => {
     const prefix = channelPrefix.getChannelPrefix()[channel.slice(1)] || PREFIX;
     const data = [];
     const { commands } = client;
     for (const [key, value] of commands.entries()) {
       if (
-        key.toLowerCase() !== "commands" &&
+        typeof value.includeInCommands === "undefined" &&
         typeof value.isModOnly === "undefined"
       ) {
         data.push(`${prefix}${key}`);
