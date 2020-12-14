378826568886d6e3e919429053005ebeadd4e7aa
Aaron Li-Mai <aaronmai@hotmail.com>
2020-12-14 10:25:18 -0700

    updated weather command

--- commands/misc/weather.js
+++ commands/misc/weather.js
@@ -1,26 +1,48 @@
-const fetch = require("node-fetch");
+const weather = require("weather-js");
+const { convert } = require("convert");
+
 module.exports = {
   commands: "weather",
   description: "Tells you KelleeLuna's current weather.",
   cooldown: 15,
   callback: (client, channel) => {
-    fetch(
-      `https://api.scorpstuff.com/weather.php?unit=imperial&city=${encodeURIComponent(
-        "New York City, New York"
-      )}`
-    )
-      .then((response) => response.text())
-      .then((data) => {
-        if (
-          data.toUpperCase() !==
-          "[ERROR: MANDATORY COOLDOWN IN EFFECT DUE TO EXCESSIVE USAGE.]"
-        ) {
-          client.say(channel, `/me ${data}.`);
+    weather.find(
+      { search: "New York City, NY", degreeType: "C" },
+      (err, result) => {
+        if (err) {
+          console.log(err);
           return;
         }
-      })
-      .catch((data) => {
-        console.log(data);
-      });
+
+        if (!result) {
+          return client.say(channel, `/me No results found.`);
+        }
+
+        const localArr = result[0];
+        if (localArr.length === 0) {
+          return client.say(channel, `/me No results found.`);
+        }
+
+        if (localArr === undefined) {
+          return;
+        }
+
+        const location = localArr.location;
+        const current = localArr.current;
+        const direction = current.winddisplay.split(" ").splice(-1)[0];
+        const fahrenheit = convert(current.temperature)
+          .from("celsius")
+          .to("fahrenheit")
+          .toFixed(1);
+        const miles = convert(current.windspeed.split(" ")[0])
+          .from("kilometers")
+          .to("miles")
+          .toFixed(1);
+
+        const text = `Weather for ${location.name}: ${current.skytext} with a temperature of ${current.temperature}°C (${fahrenheit}°F). Wind is blowing from the ${direction} at ${current.windspeed} (${miles} m/h) and the humidity is ${current.humidity}%.`;
+
+        client.say(channel, `/me ${text}`);
+      }
+    );
   },
 };
