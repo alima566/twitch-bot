9c7e7172766eb14211e721e93b2ce97741ea9559
Aaron Li-Mai <aaronmai@hotmail.com>
2021-01-26 07:50:20 -0700

    moved autoloading of commands and events to own file

--- index.js
+++ index.js
@@ -2,73 +2,60 @@ require("module-alias/register");
 require("dotenv").config();
 
 const TMI = require("tmi.js");
-const fs = require("fs").promises;
-const path = require("path");
+const { registerEvents, registerCommands } = require("@utils/registry");
+const { log } = require("@utils/utils");
 const mongoose = require("mongoose");
 
-const opts = {
-  options: {
-    debug: true,
-  },
-  connection: {
-    reconnect: true,
-    secure: true,
-    timeout: 180000,
-    reconnectDecay: 1.4,
-    reconnectInterval: 1000,
-  },
-  identity: {
-    username: process.env.BOT_USERNAME,
-    password: process.env.OAUTH_TOKEN,
-  },
-  channels: [process.env.CHANNEL_NAME],
-};
-
-const client = new TMI.client(opts);
-client.connect();
-
-client.commands = new Map();
-
-mongoose.connect(process.env.MONGO_PATH, {
-  keepAlive: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
-
-(async function registerCommands(dir = "commands") {
-  let files = await fs.readdir(path.join(__dirname, dir));
-  for (let file of files) {
-    let stat = await fs.lstat(path.join(__dirname, dir, file));
-    if (stat.isDirectory()) {
-      registerCommands(path.join(dir, file));
-    } else {
-      if (file.endsWith(".js")) {
-        let cmdName = file.substring(0, file.indexOf(".js")).toLowerCase();
-        let cmdModule = require(path.join(__dirname, dir, file));
-        client.commands.set(cmdName, cmdModule);
-      }
-    }
+(async () => {
+  try {
+    mongoose.connect(process.env.MONGO_PATH, {
+      keepAlive: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+  } catch (e) {
+    log(
+      "ERROR",
+      "./index.js",
+      `Error connecting to the database: ${e.message}.`
+    );
+    process.exit(1);
   }
-})();
 
-(async function registerEvents(dir = "events") {
-  let files = await fs.readdir(path.join(__dirname, dir));
-  for (let file of files) {
-    let stat = await fs.lstat(path.join(__dirname, dir, file));
-    if (stat.isDirectory()) {
-      registerEvents(path.join(dir, file));
-    } else {
-      if (file.endsWith(".js")) {
-        let eventName = file.substring(0, file.indexOf(".js")).toLowerCase();
-        try {
-          let eventModule = require(path.join(__dirname, dir, file));
-          console.log(`${eventName} event was registered.`);
-          client.on(eventName, eventModule.bind(null, client));
-        } catch (err) {
-          console.log(err);
-        }
-      }
-    }
+  try {
+    const opts = {
+      options: {
+        debug: true,
+      },
+      connection: {
+        reconnect: true,
+        secure: true,
+        timeout: 180000,
+        reconnectDecay: 1.4,
+        reconnectInterval: 1000,
+      },
+      identity: {
+        username: process.env.BOT_USERNAME,
+        password: process.env.OAUTH_TOKEN,
+      },
+      channels: [process.env.CHANNEL_NAME],
+    };
+
+    const client = new TMI.client(opts);
+    client.connect();
+
+    client.commands = new Map();
+
+    await registerEvents(client, "../events");
+    await registerCommands(client, "../commands");
+  } catch (e) {
+    log("ERROR", "./index.js", `An error has occurred: ${e.message}.`);
   }
+
+  log(
+    "SUCCESS",
+    "./index.js",
+    "Successfully loaded all commands, events, schemas, and connected to MongoDB."
+  );
 })();
