fe13af83fdd09e1cdee5cfe3ed40a4a69cab6e38
Aaron Li-Mai <aaronmai@hotmail.com>
2021-01-28 12:30:53 -0700

    don't include in commands command

--- commands/misc/kellee1Glare.js
+++ commands/misc/kellee1Glare.js
@@ -2,6 +2,7 @@ module.exports = {
   commands: "kellee1Glare",
   description: "KelleeBot glares back at you.",
   cooldown: 15,
+  includeInCommands: false,
   callback: (client, channel) => {
     client.say(
       channel,
