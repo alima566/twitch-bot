f90ea0de9c05fac3bbfd8869a8687a53b8f479e2
Aaron Li-Mai <aaronmai@hotmail.com>
2021-01-28 12:32:08 -0700

    don't include in commands command

--- commands/misc/uwu.js
+++ commands/misc/uwu.js
@@ -2,6 +2,7 @@ module.exports = {
   commands: "uwu",
   description: "GGs",
   cooldown: 15,
+  includeInCommands: false,
   callback: (client, channel) => {
     client.say(channel, `PrideUwu`);
     return;
